968b07a86414882e403aae20db92f1c3d4f1da2e
Eric Schweitz <eschweitz@nvidia.com>
2022-02-01 12:03:55 -0800

    The front end was folding away a function call that was an argument to the LEN intrinsic even when the result of the function call was not a constant. This left LEN(func-name) in the expression which has no semantics as the function had to be called with an argument to get the return value's LEN property at runtime.
    Update test to remove fir.convert.

--- flang/test/Lower/intrinsic-procedures/len.f90
+++ flang/test/Lower/intrinsic-procedures/len.f90
@@ -5,8 +5,7 @@ subroutine len_test(i, c)
   integer :: i
   character(*) :: c
   ! CHECK: %[[c:.*]]:2 = fir.unboxchar %arg1
-  ! CHECK: %[[xx:.*]] = fir.convert %[[c]]#1 : (index) -> i64
-  ! CHECK: %[[x:.*]] = fir.convert %[[xx]] : (i64) -> i32
-  ! CHECK: fir.store %[[x]] to %arg0
+  ! CHECK: %[[xx:.*]] = fir.convert %[[c]]#1 : (index) -> i32
+  ! CHECK: fir.store %[[xx]] to %arg0
   i = len(c)
 end subroutine
