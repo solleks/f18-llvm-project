dfb0ced4024f5773bab591f32ed5e0fcbd1ed7fb
Jean Perier <jperier@nvidia.com>
2022-02-08 12:05:54 -0800

    Fix LEN intrinsic lowering regression with arrays

--- flang/test/Lower/intrinsic-procedures/len.f90
+++ flang/test/Lower/intrinsic-procedures/len.f90
@@ -9,3 +9,14 @@ subroutine len_test(i, c)
   ! CHECK: fir.store %[[xx]] to %arg0
   i = len(c)
 end subroutine
+
+! CHECK-LABEL: len_test_array
+! CHECK-SAME: %[[arg0:.*]]: !fir.ref<i32> {fir.bindc_name = "i"}, %[[arg1:.*]]: !fir.boxchar<1> {fir.bindc_name = "c"}
+subroutine len_test_array(i, c)
+  integer :: i
+  character(*) :: c(100)
+  ! CHECK: %[[c:.*]]:2 = fir.unboxchar %[[arg1]]
+  ! CHECK: %[[xx:.*]] = fir.convert %[[c]]#1 : (index) -> i32
+  ! CHECK: fir.store %[[xx]] to %[[arg0]]
+  i = len(c)
+end subroutine
